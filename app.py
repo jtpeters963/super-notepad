0b517015a7b5c705c8dbd764209eec46b235dc80
jtpeters963 <61399650+jtpeters963@users.noreply.github.com>
2020-06-22 19:48:18 -0400

    well

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =environ.get('DATABASE_URL') or 'sqlite:///app.sqlite'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description =db.Column(db.String)
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-If __name__='__main__':
+@app.route('/tasks')
+def tasks():
+    tasks=db.session.query(Task)
+    data=[]
+    for task in tasks:
+        item ={
+            "id":task.id,
+            "description":task.description
+        }
+        data.append(item)
+if __name__=='__main__':
     app.run(debug=True)
